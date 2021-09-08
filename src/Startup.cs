9c6f45330dd64ebf420bbfc2a27ce4ec048c0be1
Mike Koenig <koenigm@seattleu.edu>
2021-09-07 21:30:44 -0700

    Removed unused using in Startup.cs

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,15 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.Json;
-using System.Threading.Tasks;
-using ContosoCrafts.WebSite.Components;
-using ContosoCrafts.WebSite.Models;
 using ContosoCrafts.WebSite.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
