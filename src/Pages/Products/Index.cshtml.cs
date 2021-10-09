a97386ae2b169199508373c4ae837732923a01d3
koenigm-seattleu <koenigm@seattleu.edu>
2021-10-09 08:39:25 -0700

    Added returning data in Products Index, updated Index.cshtml and Index.cshtml.cs

--- src/Pages/Products/Index.cshtml.cs
+++ src/Pages/Products/Index.cshtml.cs
@@ -1,16 +1,38 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.WebSite.Pages.Products
 {
+    /// <summary>
+    /// Index Page will return all the data to show
+    /// </summary>
     public class IndexModel : PageModel
     {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        /// <param name="productService"></param>
+        public IndexModel(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        // Data Service
+        public JsonFileProductService ProductService { get; }
+        
+        // Collection of the Data
+        public IEnumerable<ProductModel> Products { get; private set; }
+
+        /// <summary>
+        /// REST OnGet, return all data
+        /// </summary>
         public void OnGet()
         {
+            Products = ProductService.GetProducts();
         }
     }
 }
