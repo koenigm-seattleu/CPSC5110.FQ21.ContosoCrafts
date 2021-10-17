3846409d945d031a8fb55f411962e51cb293e03e
koenigm-seattleu <koenigm@seattleu.edu>
2021-10-17 07:52:59 -0700

    Added Quantity to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -7,16 +8,20 @@ namespace ContosoCrafts.WebSite.Models
     {
         public string Id { get; set; }
         public string Maker { get; set; }
-        
+
         [JsonPropertyName("img")]
         public string Image { get; set; }
         public string Url { get; set; }
+        
         public string Title { get; set; }
         public string Description { get; set; }
         public int[] Ratings { get; set; }
 
+
+        public string Quantity { get; set; }
+
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
- 
+
     }
 }
