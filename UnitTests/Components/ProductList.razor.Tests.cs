b90400494055c1074066a1d4ff1548be578187da
koenigm-seattleu <koenigm@seattleu.edu>
2021-10-28 18:09:32 -0700

    Adding ProductList.razor.Tests.cs to Project

---
+++ UnitTests/Components/ProductList.razor.Tests.cs
@@ -0,0 +1,39 @@
+
+using Bunit;
+using NUnit.Framework;
+
+using ContosoCrafts.WebSite.Components;
+using Microsoft.Extensions.DependencyInjection;
+using ContosoCrafts.WebSite.Services;
+using System.Linq;
+
+namespace UnitTests.Components
+{
+    public class ProductListTests : BunitTestContext
+    {
+        #region TestSetup
+
+        [SetUp]
+        public void TestInitialize()
+        {
+        }
+
+        #endregion TestSetup
+
+        [Test]
+        public void ProductList_Default_Should_Return_Content()
+        {
+            // Arrange
+            Services.AddSingleton<JsonFileProductService>(TestHelper.ProductService);
+
+            // Act
+            var page = RenderComponent<ProductList>();
+
+            // Get the Cards retrned
+            var result = page.Markup;
+
+            // Assert
+            Assert.AreEqual(false, result.Contains("jenlooper-cactus"));
+        }
+    }
+}
