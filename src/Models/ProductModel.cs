adb6b8a6bd9efc899a36e078f4afa81daa7f8f13
koenigm-seattleu <koenigm@seattleu.edu>
2021-11-07 17:38:37 -0800

    Added CommentList to ProductModel.cs

--- src/Models/ProductModel.cs
+++ src/Models/ProductModel.cs
@@ -1,6 +1,7 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 
 namespace ContosoCrafts.WebSite.Models
 {
@@ -26,6 +27,9 @@ namespace ContosoCrafts.WebSite.Models
 
         [Range (-1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
         public int Price { get; set; }
+
+        // Store the Comments entered by the users on this product
+        public List<CommentModel> CommentList { get; set; } = new List<CommentModel>();
 
         public override string ToString() => JsonSerializer.Serialize<ProductModel>(this);
 
