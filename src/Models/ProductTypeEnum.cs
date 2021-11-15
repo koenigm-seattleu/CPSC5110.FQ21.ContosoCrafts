e96ef8123a89ae9482bc8c2813f8177453c02e12
koenigm-seattleu <koenigm@seattleu.edu>
2021-11-15 14:18:12 -0800

    Adding a sample Enum to the project for the lab lecture.  Added to the models folder ProductTypeEnum.cs

---
+++ src/Models/ProductTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoCrafts.WebSite.Models
+{
+    public enum ProductTypeEnum
+    {
+        Commercial,
+        Antique,
+        Amature
+    }
+}
+
+
